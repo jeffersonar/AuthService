87ba6be6bfffa781473c811774858d2bbd1f01d5
Jefferson <rodrigues.jepherson@gmail.com>
2020-03-30 09:21:02 -0300

    configuração horuku

--- src/test/java/br/inf/datum/AuthService/service/UsuarioServiceContextTexts.java
+++ src/test/java/br/inf/datum/AuthService/service/UsuarioServiceContextTexts.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 public abstract class UsuarioServiceContextTexts {
 
     public UserCadDTO getUserCadDTO(){
-        UserCadDTO userCadDTO = new UserCadDTO();
+        UserCadDTO userCadDTO = new UserCadDTO();   
         userCadDTO.setEmail("teste@teste.com.br");
         userCadDTO.setName("teste teste");
         userCadDTO.setPassword("123");
         PhoneDTO phone = new PhoneDTO();
         phone.setDdd("51");
         phone.setNumber("88888-8888");
-        userCadDTO.setPhone(new ArrayList<>());
-        userCadDTO.getPhone().add(phone);
+        userCadDTO.setPhones(new ArrayList<>());
+        userCadDTO.getPhones().add(phone);
         return userCadDTO;
     }
 }
